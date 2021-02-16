419001e98d11c0645c7b480e88a14b5e3ceae3d1
Jonathan Rovieri <jrovieri@gmail.com>
2021-02-16 15:47:50 -0300

    vet index and controller created. closes #13

---
+++ pet-clinic-web/src/main/java/io/jrovieri/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package io.jrovieri.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+	public String listVets() {
+		return "vets/index";
+	}
+}
