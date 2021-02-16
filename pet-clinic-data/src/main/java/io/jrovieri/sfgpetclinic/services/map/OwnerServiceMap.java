5dcc1aee564db73ea4ada94f8016229b00a6e9e0
Jonathan Rovieri <jrovieri@gmail.com>
2021-02-16 15:15:11 -0300

    map based services implemented. closes #6

---
+++ pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package io.jrovieri.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import io.jrovieri.sfgpetclinic.model.Owner;
+import io.jrovieri.sfgpetclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Owner save(Owner owner) {
+		return super.save(owner.getId(), owner);
+	}
+
+	@Override
+	public void delete(Owner owner) {
+		super.delete(owner);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
---
+++ pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package io.jrovieri.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import io.jrovieri.sfgpetclinic.model.Vet;
+import io.jrovieri.sfgpetclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+	@Override
+	public Set<Vet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Vet save(Vet vet) {
+		return super.save(vet.getId(), vet);
+	}
+
+	@Override
+	public void delete(Vet vet) {
+		super.delete(vet);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
