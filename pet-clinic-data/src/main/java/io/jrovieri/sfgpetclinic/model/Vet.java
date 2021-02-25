88ba61127c966cf19cba5bf6af3c67b0b3951c21
Jonathan Rovieri <jrovieri@gmail.com>
2021-02-24 23:38:31 -0300

    specialty added. closes #28

--- pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package io.jrovieri.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
 	private static final long serialVersionUID = 1L;
 
+	private Set<Speciality> specialties;
+
+	public Set<Speciality> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Speciality> specialties) {
+		this.specialties = specialties;
+	}
 }
