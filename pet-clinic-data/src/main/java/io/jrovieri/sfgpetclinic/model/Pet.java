10bccbc650bbbfc6919ef319c1ddd16ef8413dc7
Jonathan Rovieri <jrovieri@gmail.com>
2021-03-02 08:49:59 -0300

    visit entity created

--- pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/model/Pet.java
@@ -1,10 +1,14 @@
 package io.jrovieri.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +30,9 @@ public class Pet extends BaseEntity {
 
   @Column(name = "birth_date")
   private LocalDate birthDate;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+  private Set<Visit> visits = new HashSet<>();
 
   public PetType getPetType() {
     return petType;
@@ -57,5 +64,13 @@ public class Pet extends BaseEntity {
 
   public void setBirthDate(LocalDate birthDate) {
     this.birthDate = birthDate;
+  }
+
+  public Set<Visit> getVisits() {
+    return visits;
+  }
+
+  public void setVisits(Set<Visit> visits) {
+    this.visits = visits;
   }
 }
