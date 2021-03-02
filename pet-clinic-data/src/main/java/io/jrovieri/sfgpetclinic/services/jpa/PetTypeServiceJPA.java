e5af357c39957fad1262c7ee848058231d1663b3
Jonathan Rovieri <jrovieri@gmail.com>
2021-03-02 09:48:55 -0300

    pet type jpa service added. closes #43

---
+++ pet-clinic-data/src/main/java/io/jrovieri/sfgpetclinic/services/jpa/PetTypeServiceJPA.java
@@ -0,0 +1,44 @@
+package io.jrovieri.sfgpetclinic.services.jpa;
+
+import io.jrovieri.sfgpetclinic.model.PetType;
+import io.jrovieri.sfgpetclinic.repositories.PetTypeRepository;
+import io.jrovieri.sfgpetclinic.services.PetTypeService;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetTypeServiceJPA implements PetTypeService {
+
+  private final PetTypeRepository petTypeRepository;
+
+  public PetTypeServiceJPA(PetTypeRepository petTypeRepository) {
+    this.petTypeRepository = petTypeRepository;
+  }
+
+  @Override
+  public Set<PetType> findAll() {
+    Set<PetType> petTypes = new HashSet<>();
+    petTypeRepository.findAll().forEach(petTypes::add);
+
+    return petTypes;
+  }
+
+  @Override
+  public PetType findById(Long id) {
+    return petTypeRepository.findById(id).orElse(null);
+  }
+
+  @Override
+  public PetType save(PetType object) {
+    return petTypeRepository.save(object);
+  }
+
+  @Override
+  public void delete(PetType object) {
+    petTypeRepository.delete(object);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    petTypeRepository.deleteById(id);
+  }
+}
